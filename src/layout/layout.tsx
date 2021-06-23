574539251e0e2b01de24d98b02db9f7af95b0cc5
RimlTempest <riml.slime@gmail.com>
2021-06-23 15:20:07 +0900

    update: アニメーションの実装

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,6 +1,8 @@
 import { VFC } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Backgrtound from '../components/Background';
+import particles from '../../particles';
 
 type Props = {
   children: React.ReactNode;
@@ -17,11 +19,13 @@ const Layout: VFC<Props> = (props) => {
   const onCreateLogout = () => {
     console.log('onLogout');
   };
+
   return (
     <body className="flex flex-col min-h-screen">
       <Header onCreateAccount={onCreateAccount} onLogin={onCreateLogin} onLogout={onCreateLogout} />
       <main className="flex-grow flex items-center">{props.children}</main>
       <Footer appName="NextX" name="Riml" twitterId="Fande4d" />
+      <Backgrtound options={{ ...particles }} />
     </body>
   );
 };
