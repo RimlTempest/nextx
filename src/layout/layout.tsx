78ae11d53f627ee88fbfbfca61c435159d143fc1
RimlTempest <riml.slime@gmail.com>
2021-06-24 00:38:27 +0900

    fix: 誤字を修正

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,7 +1,7 @@
 import { VFC } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Backgrtound from '../components/Background';
+import Background from '../components/Background';
 import particles from '../../particles';
 
 type Props = {
@@ -10,22 +10,12 @@ type Props = {
 
 // VFCで定義しているが実際のところJSX.Elementで良い
 const Layout: VFC<Props> = (props) => {
-  const onCreateAccount = () => {
-    console.log('onCreate');
-  };
-  const onCreateLogin = () => {
-    console.log('onLogin');
-  };
-  const onCreateLogout = () => {
-    console.log('onLogout');
-  };
-
   return (
     <body className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow flex items-center">{props.children}</main>
       <Footer appName="NextX" name="Riml" twitterId="Fande4d" />
-      <Backgrtound options={{ ...particles }} />
+      <Background options={{ ...particles }} />
     </body>
   );
 };
